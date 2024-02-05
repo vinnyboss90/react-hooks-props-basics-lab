63446123bc99b0facc168cdb8842a08fc85fa056
vinnyegon90 <vinnyegon90@gmail.com>
2024-02-06 00:45:30 +0300

    Done

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,22 @@
 import React from "react";
+import Links from "./Links.js"
+function About(props) {
+  if(!props.bio || props.bio.trim() === "") {
+    return null;
+  }
 
-function About() {
+
   return (
     <div id="about">
       <h2>About Me</h2>
       <p>Put the bio in here</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <Links
+      github = {props.github}
+      linkedin = {props.linkedin}
+
+      />
     </div>
   );
 }
